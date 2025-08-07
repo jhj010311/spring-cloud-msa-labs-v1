ecacd71e7507cd95f40c194148ecf63c79de03c4
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-08-07 15:31:26 +0900

    feat: product-service 초기 구축
    - 상품 전체 조회 기능 추가
    - 상품 단일 조회 기능 추가
    - 상품에 대한 3tier 구조 생성

---
+++ backend/product-service/src/main/java/com/sesac/productservice/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.sesac.productservice.service;
+
+import com.sesac.productservice.entity.Product;
+import com.sesac.productservice.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Product not found"));
+    }
+}
