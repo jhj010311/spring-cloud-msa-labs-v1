58708d37d1ccd83e04e111b1dd387739df17ad28
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-08-13 14:22:07 +0900

    feat: 상품 주문 이벤트용 클래스 작성

---
+++ backend/product-service/src/main/java/com/sesac/productservice/event/OrderCreatedEvent.java
@@ -0,0 +1,24 @@
+package com.sesac.productservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreatedEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
+    // 직렬화 용도
+
+    private Long orderId;
+    private Long userId;
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal totalAmount;
+    private LocalDateTime createdAt;
+}
