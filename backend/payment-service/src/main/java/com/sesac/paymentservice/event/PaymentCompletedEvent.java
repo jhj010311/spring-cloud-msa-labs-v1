a3db3761dfc90a701da76f4b2c67c32e83277409
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-08-14 14:55:38 +0900

    feat: payment-service 작동을 위한 기본틀 구성
    - 이벤트와 레포지토리 클래스 구성

---
+++ backend/payment-service/src/main/java/com/sesac/paymentservice/event/PaymentCompletedEvent.java
@@ -0,0 +1,20 @@
+package com.sesac.paymentservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentCompletedEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
+    // 직렬화 용도
+
+    private Long orderId;
+    private Long userId;
+    private BigDecimal totalAmount;
+}
