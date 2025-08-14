36c6e3f5247a3bc59839c7dd35727805f1d42cfe
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-08-14 16:39:21 +0900

    fix: order-service의 RabbitListener 오타 수정
    - 큐 명칭에 ${} 추가

--- backend/order-service/src/main/java/com/sesac/orderservice/event/OrderSagaHandler.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/event/OrderSagaHandler.java
@@ -15,17 +15,17 @@ public class OrderSagaHandler {
     // PaymentCompletedEvent
     // PaymentFailedEvent
 
-    @RabbitListener(queues = "order.event.queue.inventory-failed")
+    @RabbitListener(queues = "${order.event.queue.inventory-failed}")
     public void handleInventoryFailed(InventoryFailedEvent event) {
         log.info("재고 부족 이벤트 수신 - orderId: {}, reason: {}", event.getOrderId(), event.getReason());
     }
 
-    @RabbitListener(queues = "order.event.queue.payment-completed")
+    @RabbitListener(queues = "${order.event.queue.payment-completed}")
     public void handlePaymentCompleted(PaymentCompletedEvent event) {
         log.info("결제 성공 이벤트 수신 - orderId: {}, amount: {}", event.getOrderId(), event.getTotalAmount());
     }
 
-    @RabbitListener(queues = "order.event.queue.payment-failed")
+    @RabbitListener(queues = "${order.event.queue.payment-failed}")
     public void handlePaymentFailed(PaymentFailedEvent event) {
         log.info("결제 실패 이벤트 수신 - orderId: {}, reason: {}", event.getOrderId(), event.getReason());
     }
